77e9d33aef86a66d4f6dab21fdbf3a0b3ca5de82
shepi13 <shepi13@gmail.com>
2017-04-26 11:30:45 -0500

    Fixed small bug in display of song lengths

--- website/database.py
+++ website/database.py
@@ -59,7 +59,7 @@ def getSong(songid):
 
     seconds = songtuple[3] % 60
     minutes = songtuple[3] / 60
-    length = str(minutes) + ':' + str(seconds)
+    length = str(minutes) + ':' + ('0' if seconds < 10 else '') + str(seconds)
 
     return song.Song(songtuple[0], songtuple[1], songtuple[2], length)
 
@@ -78,7 +78,7 @@ def getSongs(query, playlistid=""):
     for row in cur.fetchall():
         minutes = row[3] / 60
         seconds = row[3] % 60
-        length = str(minutes) + ':' + str(seconds)
+        length = str(minutes) + ':' + ('0' if seconds < 10 else '') + str(seconds)
         
         songs.append(song.Song(row[0], row[1], row[2], length))
 
