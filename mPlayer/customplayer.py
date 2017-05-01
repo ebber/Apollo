d351dfc7bdddd00ee3a8a6262ecc0d89055071b0
ebber <beitel2@illinois.edu>
2017-05-01 18:21:32 -0500

    stashing

--- mPlayer/customplayer.py
+++ mPlayer/customplayer.py
@@ -77,6 +77,6 @@ if __name__ == "__main__":
   raw_input("song should resume not start over enter to pause and continue")
   file_name = raw_input("enter a song file such as songs/testSongs/Bed Of Roses.mp3 to play it")
   if file_name == "":
-    file_name = 'songs/testSongs/Bed Of Roses.mp3'
+    file_name = 'test_resources/Bed Of Roses.mp3'
   player.play_song(file_name)
   test = raw_input("should play bed of roses - hit enter to continue and end the tests")
